5ff8b9d5870971245afb3d44e75f47f113e4a014
Felipev98 <felipe_v_spd@hotmail.com>
2021-08-16 16:34:40 -0400

    add: navbar styles

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+<div class="full-height" id="banner">
+  <b-navbar toggleable="lg" >
+      <b-container>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">LOGO</b-navbar-brand>
+        <b-collapse id="nav-collapse" class="justify-content-end" is-nav>
+        <b-navbar-nav >
+        <b-nav-item href="#InicioSección" class="navbar">Inicio</b-nav-item>
+        <b-nav-item href="#Youtube" class="navbar" >Youtube</b-nav-item>
+      </b-navbar-nav>
+              <b-nav-item  class="destinos" >Comunidad</b-nav-item>
+
+          </b-collapse>
+      </b-container>
+        </b-navbar>
+        </div>
+                </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.destinos{
+    list-style: none;
+}
+</style>
